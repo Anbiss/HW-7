756a028cbfefd8acc99d7c4f532f7f5fd3c96770
Anbiss <85112270+Anbiss@users.noreply.github.com>
2022-11-27 18:51:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,120 @@
-with open('files\cookbook.txt', 'rt', encoding='utf-8') as file:
-    cook_book = {}
-    for line in file:
-        cook_name = line.strip()
-        dish_count = int(file.readline())
-        ingridients = []
-        for _ in range(dish_count):
-            ingr_list = file.readline().strip().split(' | ')
-            ingredient_name, quantity, measure = ingr_list
-            ingridients.append({'ingredient_name': ingredient_name,
-                                'quantity': int(quantity),
-                                'measure': measure})
-
-        file.readline()
-        dish = {
-            'cook_book': cook_name,
-                # 'dish_count': dish_count,
-                'ingridients': ingridients}
-        cook_book[cook_name] = ingridients
-
-print(cook_book.keys())
-print()
+# with open('files/cookbook.txt', 'rt', encoding='utf-8') as file:
+#     cook_book = {}
+#     for line in file:
+#         cook_name = line.strip()
+#         dish_count = int(file.readline())
+#         ingridients = []
+#         for _ in range(dish_count):
+#             ingr_list = file.readline().strip().split(' | ')
+#             ingredient_name, quantity, measure = ingr_list
+#             ingridients.append({'ingredient_name': ingredient_name,
+#                                 'quantity': int(quantity),
+#                                 'measure': measure})
+#
+#         file.readline()
+#         dish = {
+#             'cook_book': cook_name,
+#                 'dish_count': dish_count,
+#                 'ingridients': ingridients}
+#         cook_book[cook_name] = ingridients
+#
+# print(cook_book.keys())
+# print()
 
 #Задание 2
 
+#
 # def get_shop_list_by_dishes(dishes, person_count):
 #     res = {}
-#     # t = cook_book.keys()
-#     # print(t)
-#     for ingrid in cook_book:
-#         if ingrid == dishes:
-#             x = ingrid['ingredient_name']
-#             m = ingrid['quantity'] * person_count
-#             d = ingrid['measure']
-#             s = {'measure' : d, 'quantity' : m}
-#             res = {x : s}
-#             print(res)
-#         print(ingrid)
-#     print(cook_book)
+#     for i in dishes:
+#         for a in cook_book[i]:
+#             x = a['ingredient_name']
+#             m = a['quantity'] * person_count
+#             d = a['measure']
+#             if x not in res.keys():
+#                 res[x] = {'measure': d, 'quantity': m}
+#             elif x in res.keys():
+#                 res[x] = {'measure': d, 'quantity': m + m}
+#
+#     return (res)
+#
+#
+# print(get_shop_list_by_dishes(['Омлет', 'Фахитос'], 2))
+
+#Задание 3
+
+# with open('files/1.txt', 'rt', encoding='utf-8') as file:
+#     number_str = file.readlines()
+#     print(len(number_str))
+#     for text in file:
+#         print(text.strip())
+#
+import os
+
+current = os.getcwd()
+folder_name = 'files'
+file_name_1 = '1.txt'
+file_name_2 = '2.txt'
+file_name_3 = '3.txt'
+file_name_4 = '4.txt'
+
+full_patch_1 = os.path.join(current, folder_name, file_name_1)
+full_patch_2 = os.path.join(current, folder_name, file_name_2)
+full_patch_3 = os.path.join(current, folder_name, file_name_3)
+full_patch_4 = os.path.join(current, folder_name, file_name_4)
 
 
+#текстовый документ №1
+with open(full_patch_1, 'rt', encoding='utf-8') as file:
+    name_file_1 = '1.txt'
+    # print(name_file_1)
+    number_str_1 = file.readlines()
+    # print(len(number_str_1))
+    file.seek(0)
+    text_1 = file.read()
+    # print(text_1)
+    # file.seek(0)
+    full_1 = name_file_1, len(number_str_1), text_1
+    # print (type(full_1))
+    print()
 
-def get_shop_list_by_dishes(dishes,person_count):
-    res = {}
-    t = -1
-    while t < 5:
-        t += 1
-        for ingrid in cook_book[dishes[t]]:
-            x = ingrid['ingredient_name']
-            m = ingrid['quantity'] * person_count
-            d = ingrid['measure']
-            s = {'measure': d, 'quantity': m}
-            res[x] = s
-        # if t != dishes:
-        #     break
-        print(res)
 
-print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
-# # print(get_shop_list_by_dishes('Омлет', 2))
+#текстовый документ №2
+with open(full_patch_2, 'rt', encoding='utf-8') as file:
+    name_file_2 = '2.txt'
+    # print(name_file_2)
+    number_str_2 = file.readlines()
+    # print(len(number_str_2))
+    file.seek(0)
+    text_2 = file.read()
+    # print(text_2)
+    full_2 = name_file_2, len(number_str_2), text_2
+    # print(full_2)
+    print()
 
+
+#текстовый документ №3
+with open(full_patch_3, 'rt', encoding='utf-8') as file:
+    name_file_3 = '3.txt'
+    # print(name_file_3)
+    number_str_3 = file.readlines()
+    # print(len(number_str_3))
+    file.seek(0)
+    text_3 = file.read()
+    full_3 = name_file_3, len(number_str_3), (text_3)
+    # print(full_3)
+    # print(text_3)
+
+
+#сравнение данных в файлах
+
+
+comparison = (full_1, full_2, full_3)
+comparison = sorted(comparison, key=lambda file: file[1])
+
+
+with open(full_patch_4, 'w', encoding='utf-8') as file:
+    for a in comparison:
+        file.write(str(a[0]) + '\n')
+        file.write(str(a[1]) + '\n')
+        file.write(str(a[2]) + '\n')
